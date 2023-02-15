5dd562e1cb75edbc1b2066604aad86331db813d1
iarun18 <aruniyyappanm.cse2020@citchennai.net>
2023-02-15 19:48:11 +0530

    second commit

--- learning/keypoints.js
+++ learning/keypoints.js
@@ -9,5 +9,5 @@
 // To import modules :
 //         -> const name  = require(filepath)
 
-// Destructuring in Importing or using in:
+// Destructuring in Importing or using in  : 
 //         -> Either by { exporting names } or name.exportvalue( function(name.exportvalue))
