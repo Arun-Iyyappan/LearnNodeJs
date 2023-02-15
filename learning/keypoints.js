4cc2b95ac2c9e214928a8714b32065d60f6e25a3
iarun18 <aruniyyappanm.cse2020@citchennai.net>
2023-02-15 19:33:42 +0530

    first commit

---
+++ learning/keypoints.js
@@ -0,0 +1,13 @@
+
+// __dirname = Path to Current directory
+
+
+// To export module from the Files : 
+//         -> module.exports = {}
+
+
+// To import modules :
+//         -> const name  = require(filepath)
+
+// Destructuring in Importing or using in function:
+//         -> Either by { exporting names } or name.exportvalue( function(name.exportvalue))
