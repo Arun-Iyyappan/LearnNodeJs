b332518c5a357ceef4e9e6861ff15ad81ced0132
iarun18 <aruniyyappanm.cse2020@citchennai.net>
2023-02-16 20:09:22 +0530

    Updated

--- learning/app.js
+++ learning/app.js
@@ -1,11 +1,61 @@
 // const num = 90;
 // console.log(num);
 
-const { name , name2} = require('./names')
-const output = require('./func')
 
-output(name)
-output(name2)
+
+// const { name , name2} = require('./names')
+// const output = require('./func')
+
+
+// output(name)
+// output(name2)
+
+
+// To Read a File :  
+
+// const {readFile } = require('fs')
+
+// readFile('./text/hello.txt','utf8',(err,result) => {
+//     console.log(result);
+// })
+
+
+// HTTP Module : 
+
+// const http = require('http')
+
+// const server = http.createServer( (req,res) => {
+//     res.write('Hello World')
+//     res.end()
+// })
+
+// server.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- learning/keypoints.js
+++ learning/keypoints.js
@@ -11,3 +11,56 @@
 
 // Destructuring in Importing or using in  : 
 //         -> Either by { exporting names } or name.exportvalue( function(name.exportvalue))
+
+
+// os Module = Info about operating system -> require('os')
+
+// Path Module =  require('path') -> info about paths
+
+// fs module = require('fs') -> info about file system
+
+
+// To append contents to existing file , give third parameter { flag : 'a'} in WriteFile
+
+
+// Http Module = To create server using http.createServer(() => {})
+
+// Nested Loops Results in Blocking Codes
+
+
+// Promise Structure :
+
+// const {readFile} = require('fs')
+
+// const func = (path) => {
+//     return new Promise( (resolve,reject) => {
+//         readFile(path,'utf8',(err,data) => {
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// Using async and Await :
+// const start = async() => {
+//     try{
+//         const first = await func('./text/hello.txt')
+//         console.log(first);
+//     }
+//     catch(error){
+//          console.log(error);
+//     }
+// }
+
+// start()
+
+
+// func(path)
+//    .then( () => {})
+//    .catch( ()=> {})
+
+
